2580c0116df9b103ec994a62fd64de8dbf1f67bd
Aldy Wijaya Gustian <aldywijayag@gmail.com>
2025-02-07 18:50:25 +0700

    first commit

---
+++ kalkulatorWithInput.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Masukkan angka pertama: ', (num1) => {
+    rl.question('Masukkan operator (+ atau -): ', (operator) => {
+        rl.question('Masukkan angka kedua: ', (num2) => {
+            num1 = parseFloat(num1);
+            num2 = parseFloat(num2);
+
+            function calculator(num1, num2, operator) {
+                switch (operator) {
+                    case '+':
+                        return num1 + num2;
+                    case '-':
+                        return num1 - num2;
+                    default:
+                        return 'Operator tidak valid';
+                }
+            }
+
+            console.log(`Hasil: ${calculator(num1, num2, operator)}`);
+            rl.close();
+        });
+    });
+});
---
+++ sesi4_AldyWijayaGustian.js
@@ -0,0 +1,14 @@
+function kalkulator(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return `Penjumlahan: ${num1} + ${num2} = ${num1 + num2}`;
+        case '-':
+            return `Pengurangan: ${num1} - ${num2} = ${num1 - num2}`;
+        default:
+            return `Operator ${operator}: Operator tidak valid`;
+    }
+}
+
+console.log(kalkulator(15, 5, '+'));
+console.log(kalkulator(25, 10, '-'));
+console.log(kalkulator(10, 5, '*'));
